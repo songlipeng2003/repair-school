09d2fc8f5b21d9a248b6118e7b9ca0bd6a5b5e81
Thinking Song <123wuming_123@163.com>
2017-05-10 01:15:52 +0800

    Add vue resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App'
+
+Vue.http.options.root = 'http://127.0.0.1'
+Vue.http.headers.common['X-Access-Token'] = '1'
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
