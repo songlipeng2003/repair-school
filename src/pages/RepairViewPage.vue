4bcfcd1aee18e9d13164d7b047f73542598ecf9d
Thinking Song <123wuming_123@163.com>
2017-05-19 00:28:02 +0800

    Update repair list

--- src/pages/RepairViewPage.vue
+++ src/pages/RepairViewPage.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <mt-header :title="repair.title">
+    <mt-header :title="repair.code">
       <router-link to="/repair" slot="left">
         <mt-button icon="back"></mt-button>
       </router-link>
     </mt-header>
 
-    <p>{{ repair.content }}</p>
+    <mt-cell title="名称" :value="repair.name"></mt-cell>
+    <mt-cell title="密码" :value="repair.code"></mt-cell>
+    <mt-cell title="品牌" :value="repair.brand"></mt-cell>
+    <mt-cell title="型号" :value="repair.model"></mt-cell>
+    <mt-cell title="位置" :value="repair.position"></mt-cell>
+    <mt-cell title="数量" :value="repair.quantity"></mt-cell>
+    <mt-cell title="单位" :value="repair.union"></mt-cell>
+    <mt-cell title="创建时间" :value="repair.created_at"></mt-cell>
+    <mt-cell title="备注" :value="repair.remark"></mt-cell>
   </div>
 </template>
 
 <script>
 import { Header, Cell, Button } from 'mint-ui'
 
-import { Announcement } from '../resources'
+import { Repair } from '../resources'
 
 export default {
   components: {
@@ -28,7 +36,7 @@ export default {
     }
   },
   beforeMount () {
-    Announcement.get({id: this.id}).then((response) => {
+    Repair.get({id: this.id}).then((response) => {
       this.repair = response.body
     })
   }
